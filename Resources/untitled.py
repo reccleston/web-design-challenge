9f02d2de1ff7e2820207d1c716fb88506788f695
Ryan EM <ryan.eccleston.murdock@gmail.com>
2021-02-06 16:52:56 -0500

    added data tables

---
+++ Resources/untitled.py
@@ -0,0 +1,10 @@
+asteroid_specs = {
+    'day': [],
+    'id': [],
+    'asteroid_name': [],
+    'min_diam_m': [],
+    'max_diam_m': [],
+    'min_diam_f': [],
+    'max_diam_f': [],
+    'hazard_to_earth': []
+}
